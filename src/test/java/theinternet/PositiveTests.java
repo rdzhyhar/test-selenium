3de3fd0c9ceb0736ad4bded2e47a31a53525d9ab
rdzhyhar <dgigarroma@gmail.com>
2022-01-20 16:08:41 +0200

    create PositiveTests class

---
+++ src/test/java/theinternet/PositiveTests.java
@@ -0,0 +1,22 @@
+package theinternet;
+
+public class PositiveTests {
+
+    public void loginTest() {
+        //Create driver
+
+        //open test page
+
+        //enter username
+
+        //enter password
+
+        //clock login button
+
+        //verification:
+
+
+
+    }
+
+}
