59f39bf9213f7ab6e55bfaf6a4915bbecc15432a
rdzhyhar <dgigarroma@gmail.com>
2022-01-20 16:46:18 +0200

    loginTest

--- src/test/java/theinternet/PositiveTests.java
+++ src/test/java/theinternet/PositiveTests.java
@@ -1,12 +1,23 @@
 package theinternet;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class PositiveTests {
 
+    @Test
     public void loginTest() {
+        System.out.println("Starting loginTest");
         //Create driver
-
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        //maximize driver window
+        driver.manage().window().maximize();
         //open test page
-
+        String url = "http://the-internet.herokuapp.com/login";
+        driver.get(url);
+        System.out.println("Page is opened");
         //enter username
 
         //enter password
@@ -14,9 +25,14 @@ public class PositiveTests {
         //clock login button
 
         //verification:
+        //new url
+        //logout button is visible
+        //successful login message
 
-
+        //Close browser
+        driver.quit();
 
     }
+
 
 }
