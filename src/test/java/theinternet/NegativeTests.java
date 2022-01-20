05c7c922912d7a1dc5e4d28aae7e4cc4f993d771
rdzhyhar <dgigarroma@gmail.com>
2022-01-20 20:34:53 +0200

    negativeUsernameTest

---
+++ src/test/java/theinternet/NegativeTests.java
@@ -0,0 +1,47 @@
+package theinternet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NegativeTests {
+    @Test
+    public void negativeUsernameTest() {
+        System.out.println("Starting negativeUsernameTest");
+        //Create driver
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        //maximize driver window
+        driver.manage().window().maximize();
+        //open test page
+        String url = "https://the-internet.herokuapp.com/login";
+        driver.get(url);
+        System.out.println("Page is opened");
+        //enter username
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("incorrect");
+        //enter password
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("SuperSecretPassword!");
+        //click login button
+        WebElement logInButton = driver.findElement(By.tagName("button"));
+        logInButton.click();
+        //verifications:
+        //successful login message
+        WebElement errorMessage = driver.findElement(By.id("flash"));
+        String expectedErrorMessage = "Your username is invalid!";
+        String actualErrorMessage = errorMessage.getText();
+        Assert.assertTrue
+                (actualErrorMessage.contains(expectedErrorMessage),
+                        "Actual message does not contains expected message." +
+                                "\nActual message: " + actualErrorMessage +
+                                "\nExpected message: " + expectedErrorMessage);
+        //Close browser
+        driver.quit();
+
+    }
+
+}
