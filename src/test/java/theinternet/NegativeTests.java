fa66b6fd441c4a45efc8ea00e6a647788d766d5e
rdzhyhar <dgigarroma@gmail.com>
2022-01-21 16:54:19 +0200

    negativePasswordTest

--- src/test/java/theinternet/NegativeTests.java
+++ src/test/java/theinternet/NegativeTests.java
@@ -22,7 +22,7 @@ public class NegativeTests {
         System.out.println("Page is opened");
         //enter username
         WebElement username = driver.findElement(By.id("username"));
-        username.sendKeys("incorrect");
+        username.sendKeys("incorrectUsername");
         //enter password
         WebElement password = driver.findElement(By.name("password"));
         password.sendKeys("SuperSecretPassword!");
@@ -33,6 +33,42 @@ public class NegativeTests {
         //successful login message
         WebElement errorMessage = driver.findElement(By.id("flash"));
         String expectedErrorMessage = "Your username is invalid!";
+        String actualErrorMessage = errorMessage.getText();
+        Assert.assertTrue
+                (actualErrorMessage.contains(expectedErrorMessage),
+                        "Actual message does not contains expected message." +
+                                "\nActual message: " + actualErrorMessage +
+                                "\nExpected message: " + expectedErrorMessage);
+        //Close browser
+        driver.quit();
+
+    }
+
+    @Test
+    public void negativePasswordTest() {
+        System.out.println("Starting negativePasswordTest");
+        //Create driver
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        //maximize driver window
+        driver.manage().window().maximize();
+        //open test page
+        String url = "https://the-internet.herokuapp.com/login";
+        driver.get(url);
+        System.out.println("Page is opened");
+        //enter username
+        WebElement username = driver.findElement(By.id("username"));
+        username.sendKeys("tomsmith");
+        //enter password
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("incorrectPassword");
+        //click login button
+        WebElement logInButton = driver.findElement(By.tagName("button"));
+        logInButton.click();
+        //verifications:
+        //successful login message
+        WebElement errorMessage = driver.findElement(By.id("flash"));
+        String expectedErrorMessage = "Your password is invalid!";
         String actualErrorMessage = errorMessage.getText();
         Assert.assertTrue
                 (actualErrorMessage.contains(expectedErrorMessage),
