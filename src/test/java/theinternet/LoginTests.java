a40d32e3add2780c7b677e7461cb9c7fca7cef82
rdzhyhar <dgigarroma@gmail.com>
2025-01-16 17:07:25 +0200

    test1

--- src/test/java/theinternet/LoginTests.java
+++ src/test/java/theinternet/LoginTests.java
@@ -38,7 +38,6 @@ public class LoginTests {
         //maximize driver window
         driver.manage().window().maximize();
         //implicitWait
-        //test
         //qwe
     }
         //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
